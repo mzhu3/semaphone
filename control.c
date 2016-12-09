c1bf26311222a96dadaf5633fa12bddbeb706c5d
Matthew Zhu <mzhu3@stuy.edu>
2016-12-09 11:47:42 -0500

    finished control

--- control.c
+++ control.c
@@ -5,6 +5,8 @@
 #include <sys/sem.h>
 #include <fcntl.h>
 #include <sys/shm.h>
+#include <unistd.h>
+
 union semun{
   int value;
   struct semid_ds *buf;
@@ -12,15 +14,34 @@ union semun{
   struct seminfo *_buf;
 };
 
-int main(){
+int main(int argc, char *argv[]){
   char command[256];
   int sc;
-  fgets(command,sizeof(command),stdin);
-  int semid = semget(ftok("story.txt",256),1, IPC_CREAT| 0644);
-  open("story.txt", O_TRUNC | O_CREAT, 0644);
-  union semun su;
-  su.value = 1;
-  sc = semctl(semid,0,SETVAL,su);
-  shmget(24333, sizeof(char), IPC_CREAT | 0644);
+  int meme = shmget(24333, sizeof(char), IPC_CREAT | 0644);
+  if(strncmp(argv[1], "-c", strlen(argv[1]))==0){
+    int semid = semget(ftok("story.txt",256),1, IPC_CREAT| 0644);
+    open("story.txt", O_TRUNC | O_CREAT, 0644);
+    printf("semaphore created, storytxt created\n");
+    union semun su;
+    su.value = 1;
+    sc = semctl(semid,0,SETVAL,su);
+  }
+  else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
+    int  semid = semget(ftok("story.txt",256), 1, 0);
+    sc = semctl(semid, 0, GETVAL);
+    printf("semaphore value: %d\n",sc);
+    char *s[100] = {"cat","story.txt"};
+    execvp(s[0],s);
+
+  }
+  else if(strncmp(argv[1], "-r", strlen(argv[1])) == 0){
+    int semid = semget(ftok("story.txt",256), 1, 0);
+    sc = semctl(semid, 0, IPC_RMID);
+    printf("semaphore removed: %d\n", sc);
+    char *s[100] = {"cat","story.txt"};
+    execvp(s[0],s);
+  }
+
+  
   return 0;
 }
